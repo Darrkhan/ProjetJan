dfb40716858f060959510a5808e8678bdef585e3
Lionel SMETS <lionel.smets@isen.yncrea.fr>
2022-01-05 11:19:06 +0100

    drag and drop file explorer

--- src/pages/components/fileExplorer.js
+++ src/pages/components/fileExplorer.js
@@ -8,41 +8,55 @@ class TheFileExplorer extends React.Component {
   state = {
     files: [
       {
-        key: 'photos/animals/cat in a hat.png',
+        key: 'database/animals/cat in a hat.wav',
         modified: +Moment().subtract(1, 'hours'),
         size: 1.5 * 1024 * 1024,
       },
       {
-        key: 'photos/animals/kitten_ball.png',
-        modified: +Moment().subtract(3, 'days'),
-        size: 545 * 1024,
-      },
-      {
-        key: 'photos/animals/elephants.png',
+        key: 'database/animals/elephants.avi',
         modified: +Moment().subtract(3, 'days'),
         size: 52 * 1024,
       },
       {
-        key: 'photos/funny fall.gif',
+        key: 'database/funny fall.mp4',
         modified: +Moment().subtract(2, 'months'),
         size: 13.2 * 1024 * 1024,
       },
       {
-        key: 'photos/holiday.jpg',
+        key: 'database/holiday.mp4',
         modified: +Moment().subtract(25, 'days'),
         size: 85 * 1024,
       },
       {
-        key: 'documents/letter chunks.doc',
+        key: 'current/letter chunks.avi',
         modified: +Moment().subtract(15, 'days'),
         size: 480 * 1024,
       },
       {
-        key: 'documents/export.pdf',
+        key: 'current/export.mp4',
         modified: +Moment().subtract(15, 'days'),
         size: 4.2 * 1024 * 1024,
       },
     ],
+  }
+
+  handleRenameFile = (oldKey, newKey) => {
+    this.setState(state => {
+      const newFiles = []
+      state.files.map((file) => {
+        if (file.key === oldKey) {
+          newFiles.push({
+            ...file,
+            key: newKey,
+            modified: +Moment(),
+          })
+        } else {
+          newFiles.push(file)
+        }
+      })
+      state.files = newFiles
+      return state
+    })
   }
 
   render() {
@@ -50,6 +64,7 @@ class TheFileExplorer extends React.Component {
       <FileBrowser
         files={this.state.files}
         icons={Icons.FontAwesome(4)}
+        onMoveFile={this.handleRenameFile}
       />
     )
   }
